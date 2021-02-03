53abcdf4640d4e5c901f2a5ddcb6518a4f6a5fa3
Miguel22247 <minipachru@gmail.com>
2021-02-03 15:49:07 +0000

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-        puts("\"Programing is like building a multilingual puzzle");
-        return (0);
+puts("\"Programing is like building a multilingual puzzle");
+return (0);
 }
