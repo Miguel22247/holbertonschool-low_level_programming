c236a1f30ae2743cb27565c3fa4ae5e64ef8158e
Miguel22247 <minipachru@gmail.com>
2021-02-03 16:06:00 +0000

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+B#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-puts("\"Programing is like building a multilingual puzzle");
+puts("\"Programing is like building a multilingual puzzle\n");
 return (0);
 }
