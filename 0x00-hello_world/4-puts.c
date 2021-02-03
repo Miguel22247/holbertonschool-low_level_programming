ecd14ead9687769faf14b87a8ca0c5342a52e925
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-03 13:12:35 -0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@ D#include <stdio.h>
  */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
