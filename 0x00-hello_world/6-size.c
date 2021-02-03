4ef03c92770a58554044d367fab1965163a56b30
Miguel22247 <minipachru@gmail.com>
2021-02-03 16:04:53 +0000

    test

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;  
+float f;
+
+ printf("
+}
