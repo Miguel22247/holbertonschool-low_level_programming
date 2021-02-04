727ef15a67e815ddc83892761e291a48f6440fe3
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-04 15:05:16 -0300

    Update 10-print_comb2.c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -8,19 +8,19 @@
  */
 int main(void)
 {
-  int d;
+	int d;
 
-  for (d = 0; d < 100; d++)
-      {
-	putchar((d / 10) + '0');
-	putchar((d % 10) + '0');
-	if (d != '99')
-	  {
-	    putchar(',');
-	    putchar(' ');
-	  }
-      }
-      putchar('\n');
+	for (d = 0; d < 100; d++)
+	{
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+		if (d != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 
-      return (0);
-    }
+	return (0);
+}
