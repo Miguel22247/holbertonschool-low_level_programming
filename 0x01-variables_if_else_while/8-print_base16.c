7e4bcd9a829ba287d7c5726f3cca9707d2fb676a
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-04 14:49:33 -0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -8,14 +8,14 @@
  */
 int main(void)
 {
-  int c;
-  char low;
+	int d;
+	char low;
 
-  for (c = '0'; c <='9'; c++)
-    putchar(c);
-  for (low = 'a'; low <= 'f'; low++)
-    putchar(low);
-  putchar('\n');
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+		putchar('\n');
 
-  return(0);
- }
+	return (0);
+}
