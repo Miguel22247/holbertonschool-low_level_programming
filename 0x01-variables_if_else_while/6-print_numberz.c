2303ae6a1886023b6dc78448b618d0b9d9dddfe4
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-04 14:20:56 -0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,9 +8,11 @@
  */
 int main(void)
 {
-  int d;
-  for (d = 0; d <= 9; d++)
-    putchar (d);
-  putchar('\n');
-  return (0);
- }
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+		putchar('\n');
+
+	return (0);
+}
