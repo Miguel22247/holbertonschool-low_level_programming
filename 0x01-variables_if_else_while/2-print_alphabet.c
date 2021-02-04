dc7299b4e588c3dacaa0e2780d364e4458425330
Miguel22247 <minipachru@gmail.com>
2021-02-04 16:24:32 +0000

    test

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+  char low;
+
+  for (low ='a'; low <= 'z'; low++)
+    putchar (low);
+  printf("\n");
+
+  return (0);
+}
