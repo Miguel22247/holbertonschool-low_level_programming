d1f9a3232e1746b7cbf8e075ff4733611ad16932
Miguel22247 <minipachru@gmail.com>
2021-02-04 17:43:58 +0000

    test

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-  char low;
-  for (low = 'z'; low <= 'a'; low--)
-    putchar(low);
-  putchar('\n');
+char low;
+for (low = 'z'; low >= 'a'; low--)
+putchar(low);
+putchar('\n');
 
-  return (0);
- }
+return (0);
+}
