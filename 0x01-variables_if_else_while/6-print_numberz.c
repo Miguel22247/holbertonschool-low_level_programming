094452614c0db60c868e64f0aeb6e77057e80be8
Miguel22247 <minipachru@gmail.com>
2021-02-04 17:19:28 +0000

    test

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int d;
+  for (d = 0; d <= 9; d++)
+    putchar (d);
+  putchar('\n');
+  return (0);
+ }
