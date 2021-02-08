1b1e1042ab8e43f45ae1172194ec90a539301cc5
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-08 15:11:16 -0300

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+void print_alphabet(void)
+{
+  int n;
+
+  for (n = 'a'; n <= 'z'; n++)
+    {
+      _putchar(n);
+    }
+  _putchar('\n');
+}
