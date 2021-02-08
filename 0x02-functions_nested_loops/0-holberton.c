57826b94d6a7349bc6c4fedb24c108345b23522e
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-08 14:47:09 -0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,5 +7,16 @@
 int main(void)
 {
 
+  _putchar('H');
+  _putchar('o');
+  _putchar('l');
+  _putchar('b');
+  _putchar('e');
+  _putchar('r');
+  _putchar('t');
+  _putchar('o');
+  _putchar('n');
+ _putchar('\n');
+
   return (0);
 }
