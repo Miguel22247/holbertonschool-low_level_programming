15dfe872f5ce42efed9a71b8e1c8e0ab000aea08
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-09 09:46:10 -0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,20 +4,19 @@
  *
  * Return: x10 a-z
  */
-
 void print_alphabet_x10(void)
 {
-int n, co;
+	int n, co;
 
-co = 0;
+	co = 0;
 
-while (co < 10)
-{
-for (n = 'a'; n <= 'z'; n++)
-{
-_putchar(n);
-}
-co++;
-_putchar('\n');
-}
+	while (co < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		co++;
+		_putchar('\n');
+	}
 }
