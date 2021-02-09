faae18f89d1eeacf07548c10cf21f3b3798f255a
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-09 13:56:41 -0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,14 +11,13 @@ int main(void)
         printf("Infinite loop incoming :(\n");
 
         i = 0;
+/*
+ * while (i < 10)
+ * {
+ *  putchar(i);
+ *  }
+*/
+	printf("Infinite loop avoided! \\o/\n");
 
-        while (i < 10)
-        {
-                putchar(i);
-		i++;
-        }
-
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+	return (0);
 }
