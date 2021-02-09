095554544f620cb3d7f7166d97d8dfe77cc6d645
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-09 11:29:32 -0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,15 +6,15 @@
  */
 int print_last_digit(int nld)
 {
-  int pld;
+	int pld;
 
-  pld = (nld % 10);
+	pld = (nld % 10);
 
-  if (pld < 0)
-    {
-      pld = (-1 * pld);
-    }
+	if (pld < 0)
+	{
+		pld = (-1 * pld);
+	}
 
-  _putchar(pld + '0');
-  return (pld);
+	_putchar(pld + '0');
+	return (pld);
 }
