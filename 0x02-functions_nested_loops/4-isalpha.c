024cebacfd63180ff196c415940619bef786a047
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-09 10:36:20 -0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,8 +6,8 @@
  */
 int _isalpha(int c)
 {
-  if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
-    return (1);
-  else
-    return (0);
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		return (1);
+	else
+		return (0);
 }
