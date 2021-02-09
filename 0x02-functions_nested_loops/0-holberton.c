1593282244f2e5775f4a61a7ba9d395ce6825a28
Miguel22247 <minipachru@gmail.com>
2021-02-09 12:27:04 +0000

    test

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 
-  _putchar('H');
-  _putchar('o');
-  _putchar('l');
-  _putchar('b');
-  _putchar('e');
-  _putchar('r');
-  _putchar('t');
-  _putchar('o');
-  _putchar('n');
- _putchar('\n');
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 
-  return (0);
+return (0);
 }
