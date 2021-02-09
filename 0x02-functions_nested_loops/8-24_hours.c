be4552e43be46c9e544ba7311fd24d51af5d2621
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-09 12:16:47 -0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,18 +8,18 @@
  */
 void jack_bauer(void)
 {
-  int h, m;
+	int h, m;
 
-  for (h = 0; h < 24; h++)
-    {
-      for (m = 0; m < 60; m++)
+	for (h = 0; h < 24; h++)
 	{
-	  _putchar((h / 10) + '0');
-	  _putchar((h % 10) + '0');
-	  _putchar(':');
-	  _putchar((m / 10) + '0');
-	  _putchar((m % 10) + '0');
-	  _putchar('\n');
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
 	}
-    }
 }
