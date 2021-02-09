75c62dab05a80aaf45ef66df1edf34dabccaf24f
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-09 09:45:29 -0300

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
+
+void print_alphabet_x10(void)
+{
+int n, co;
+
+co = 0;
+
+while (co < 10)
+{
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
+co++;
+_putchar('\n');
+}
+}
