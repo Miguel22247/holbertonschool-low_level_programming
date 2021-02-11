25c0e293d0e6a1a8a52a4cc6100ae5efd5c718f3
Miguel22247 <minipachru@gmail.com>
2021-02-11 16:12:59 +0000

    test

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,17 +7,18 @@
  */
 void print_line(int n)
 {
-
 	int co;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for(co = 1; co <= n; co++)
+		for (co = 1; co <= n; co++)
+		{
 			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-}
 }
