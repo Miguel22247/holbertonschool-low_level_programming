4af3c699e1cc465bd4f9d14c2ee0a99502755734
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-11 12:46:36 -0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,20 +5,20 @@
  * _putchar only 3 times
  * Return: 0-14 x10 followed by new line
  */
-void more_numbers(void);
-
-int n, ro;
-
-for(ro = 0; ro <= 10; ro++)
+void more_numbers(void)
 {
-	for(n = 0; n = 14; n++)
+	int i, ro;
+
+	for (ro = 0; ro < 10; ro++)
 	{
-		if (n >= 10)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar((n / 10) + '0');
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
 		}
-		_putchar((n % 10) + '0');
+		_putchar('\n');
 	}
-	_putchar('\n');
-}
 }
