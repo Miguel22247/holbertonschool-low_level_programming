9476fb116be14c935da0354ce3567fac64473460
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-11 13:55:08 -0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
-
-
-
-
+/**
+ * print_square - a function that prints a square, followed by a new line
+ * @size: size of both width and length
+ * Return: a square made of '#'
+ */
 void print_square(int size)
 {
 
