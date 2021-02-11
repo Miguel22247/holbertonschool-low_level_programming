e580f5152266000d35733cf809d4e194cbfdb1d7
Miguel22247 <minipachru@gmail.com>
2021-02-11 13:47:17 +0000

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+	for (n = 0; n <= 9; n++)
+	{
+		if ((n == 2) || (n == 4))
+			continue;
+		_putchar(i + '0');
+		_putchar('\n');
+	}
+}
