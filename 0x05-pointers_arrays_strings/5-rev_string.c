8ec2d0c97bf3a120bff90fe47182fd29bece2d7e
Miguel22247 <minipachru@gmail.com>
2021-02-13 13:57:04 +0000

    test

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,23 @@
 #include "holberton.h"
-
 /**
- * rev_string - reverses the string
+ * rev_string - a function that reverses a string
+ * @s: input to reverse
+ * Return: string and in reverse
  */
-void rev_string(char *s);
-{
-	int 
-}
+	void rev_string(char *s)
+	{
+		char rev = s[0];
+		int fcounter = 0;
+		int i;
+
+		while (s[fcounter] != '\0')
+			fcounter++;
+
+		for (i = 0; i < fcounter; i++)
+		{
+			fcounter--;
+			rev = s[i];
+			s[i] = s[fcounter];
+			s[fcounter] = rev;
+		}
+	}
