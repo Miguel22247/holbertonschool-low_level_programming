201ea5c088408a65ac23dea3be60796c24935b70
Miguel22247 <minipachru@gmail.com>
2021-02-13 14:22:55 +0000

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int n = 0;
+	for (; str[n] != '\0'; n++)
+	{
+		if ((n % 2) == 0)
+			_putchar(str[n]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
