faa977a7ea3dd944a4055aff105bd228bf166326
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-15 16:50:51 -0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
 
 /**
- * *_strcpy - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-
-        for (; src[i] != '\0'; i++)
-        {
-                dest[i] = src[i];
-        }
-        return (dest);
+	char *saved = dest;
+	while (*src)
+	{
+		*dest++ = *src++;
+	}
+	*dest = 0;
+	return saved;
 }
