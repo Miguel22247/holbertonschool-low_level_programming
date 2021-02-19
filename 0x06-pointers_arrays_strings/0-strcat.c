fe07e8f6e5b351219b8ea305b3c77c3e575dbce1
Miguel22247 <minipachru@gmail.com>
2021-02-19 13:39:14 +0000

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,12 +11,12 @@ char *_strcat(char *dest, char *src)
 
 	int i, n;
 
-	for (i =0; dest[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
 
 	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
 	{
 	}
-	return(dest);
+	return (dest);
 }
