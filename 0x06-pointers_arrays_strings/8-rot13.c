9e81cd12a772b08cf6dcaf0e9354859b22e04135
Miguel22247 <minipachru@gmail.com>
2021-02-19 18:08:25 +0000

    test

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * rot13 - Entry point
+ * ONE if, TWO loops only...
+ * @n: input
+ * Return: decrypted string
+ */
+char *rot13(char *n)
+{
+	int x, rot_c = 13, i = 0;
+	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
+			 'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
+			 's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
+			 'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
+			 'Z', 'm', 'z'};
+
+	while (n[i] != '\0')
+	{
+		for (x = 0; x <= 51; x++)
+		{
+			if (n[i] == toswap[x])
+			{
+				n[i] = n[i] + rot_c;
+				x = 51;
+			}
+			rot_c = rot_c * -1;
+		}
+		i++;
+	}
+	return (n);
+}
