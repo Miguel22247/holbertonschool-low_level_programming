51511835dcbb5174cc3a7d803c0c68497cd3e0da
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-19 16:04:09 -0300

    Update 8-rot13.c

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,32 +1,27 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
- * rot13 - Entry point
- * ONE if, TWO loops only...
- * @n: input
- * Return: decrypted string
+ * rot13 - qwqerwqerwqerwqer
+ * @s: werwqreqwerwqerw
+ * Return:sadfsadf
  */
-char *rot13(char *n)
+char *rot13(char *s)
 {
-	int x, rot_c = 13, i = 0;
-	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
-		'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
-		's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
-		'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
-		'Z', 'm', 'z'};
+	int i = 0, i2 = 0;
+	char my[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char my2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (n[i] != '\0')
+	while (*(s + i) != 0)
 	{
-		for (x = 0; x <= 51; x++)
+		for (i2 = 0; i2 <= 52; i2++)
 		{
-			if (n[i] == toswap[x])
+			if (*(s + i) == my[i2])
 			{
-				n[i] = n[i] + rot_c;
-				x = 51;
+				*(s + i) = my2[i2];
+				break;
 			}
-			rot_c = rot_c * -1;
 		}
-		i++;
+				i++;
 	}
-	return (n);
+		return (s);
 }
