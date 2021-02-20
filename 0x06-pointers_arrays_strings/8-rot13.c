26ae68fc3a34a7b53ec335fb89fb79133b13230e
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-20 06:26:36 -0300

    Update

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -8,16 +8,16 @@
 char *rot13(char *s)
 {
 	int i = 0, i2 = 0;
-	char my[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char my2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alpha2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	while (*(s + i) != 0)
 	{
 		for (i2 = 0; i2 <= 52; i2++)
 		{
-			if (*(s + i) == my[i2])
+			if (*(s + i) == alpha[i2])
 			{
-				*(s + i) = my2[i2];
+				*(s + i) = alpha2[i2];
 				break;
 			}
 		}
