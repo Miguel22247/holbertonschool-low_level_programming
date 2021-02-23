01ed9fcf5e43506be58cafa761407563e14291f8
Miguel22247 <minipachru@gmail.com>
2021-02-23 13:27:25 +0000

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - a function that copy memory area
+ * @dest: copy to
+ * @src: copy from
+ * @n: bytes
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
