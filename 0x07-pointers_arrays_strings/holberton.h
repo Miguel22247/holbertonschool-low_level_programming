a144de16b0bf2e6e0d84c09f7f7344dfa127bda2
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-23 14:12:58 -0300

    Update holberton.h

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -8,3 +11,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
+#endif /* HOLBERTON_H */
