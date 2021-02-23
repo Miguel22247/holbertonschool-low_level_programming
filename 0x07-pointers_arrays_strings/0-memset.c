80e35a274f4748f07bafbd49b0a8a153b232adec
Miguel22247 <minipachru@gmail.com>
2021-02-23 15:07:44 +0000

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 
 /**
- * _memset - fills the first n bytes of the memory area
- * @b: constant bytes
+ * _memset - Entry point
  * @s: pointed destination
+ * @b: constant byte
  * @n: bytes
+ * Return: Always 0 (Success)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 		s[i] = b;
 	return (s);
