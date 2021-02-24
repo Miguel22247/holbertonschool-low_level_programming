239d1978cd906fc090d6be386ac0f9a14ae93a93
Miguel22247 <minipachru@gmail.com>
2021-02-24 13:35:35 +0000

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - Entry point
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+		_print_rev_recursion(s+1);
+	else
+		return;
+	_putchar(*s);
+}
