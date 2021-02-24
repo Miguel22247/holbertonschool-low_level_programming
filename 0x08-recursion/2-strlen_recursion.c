5625c5453148803f38d4a5950357c8b581dd0551
Miguel Pacheco <66649043+Miguel22247@users.noreply.github.com>
2021-02-24 13:20:12 -0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
 /**
- * _strlen_recursion - Entry point
+ * _strlen_recursion - note: no _putchar in local test
  * @s: input
  * Return: Always 0 (Success)
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return(0);
+		return (0);
 	else
 		return (1 + _strlen_recursion(s + 1));
 }
