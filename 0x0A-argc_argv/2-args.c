477a6f4b50ade97eb96174aeecfd14000f156a31
Miguel22247 <minipachru@gmail.com>
2021-02-26 17:43:24 +0000

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+  int i = 0;
+
+  while (i < argc)
+    {
+      printf("%s\n", argv[i]);
+      i++;
+    }
+  return (0);
+}
