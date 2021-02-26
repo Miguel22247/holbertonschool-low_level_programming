6da31c54ed55d87a9f8f57f770cdb841750e70d2
Miguel22247 <minipachru@gmail.com>
2021-02-26 18:04:16 +0000

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+  int i, sum = 0;
+
+  if (argc < 1)
+    return (0);
+
+  for (i = 0; i < argc; i++)
+    {
+      if (!atoi(argv[i]))
+	{
+	  printf("%s\n", "Error");
+	  return (1);
+	}
+      sum += atoi(argv[i]);
+    }
+  printf("%d\n", sum);
+
+  return (0);
+}
