535ae38d24c872e8d64181b054bc33af5ea8e77b
Miguel Pacheco <minipachru@gmail.com>
2021-03-01 12:11:35 -0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include "holberton.h"
-
 /**
  * main - prints its name + \n
  * @argc: number of arguments typed
  * @argv: array pointing to arguements
  * Return: 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  argc = 0;
+	argc = 0;
 
-  printf("%s\n", argv[argc]);
-  return (0);
+	printf("%s\n", argv[argc]);
+	return (0);
 }
