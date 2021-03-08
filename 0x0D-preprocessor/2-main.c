1153f3784ff9d1acf95c74e731f8ab0999ce2c13
Miguel22247 <minipachru@gmail.com>
2021-03-08 10:13:28 -0300

    test

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
-#include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
 /**
  * main - Entry Point
  * Return: 0
  */
 int main(void)
 {
-    printf("%s\n",__FILE__)
-    return(0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
