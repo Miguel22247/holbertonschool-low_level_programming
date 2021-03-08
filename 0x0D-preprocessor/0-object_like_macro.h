5632ffe9c6096ef223e722b56e9bde131cb92795
Miguel22247 <minipachru@gmail.com>
2021-03-08 12:26:00 -0300

    test

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,4 @@
 #ifndef OBJECT_LIKE_MACRO
 #define OBJECT_LIKE_MACRO
-
 #define SIZE 1024
-
-#endif /* OBJECT_LIKE_MACRO */
+#endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,4 @@
 #ifndef SUM
 #define SUM
-
 #define SUM(x, y) ((x + y))
-
 #endif
