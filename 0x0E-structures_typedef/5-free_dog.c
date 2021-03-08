5898c3fd46e284a6f92d30126a401f63ddbce99c
Miguel22247 <minipachru@gmail.com>
2021-03-08 18:07:14 +0000

    update code

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,11 +7,11 @@
 
 void free_dog(dog_t *d)
 {
-    if (d == 0)
-		    return;
-    if (d->name)
-            free(d->name);
-    if (d->owner)
-            free(d->owner);
+	if (d == 0)
+		return;
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
 	free(d);
 }
