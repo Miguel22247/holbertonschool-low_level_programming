312ae4e50dcc3f56a154825a483d5c52cfa9c132
Miguel22247 <minipachru@gmail.com>
2021-03-09 14:21:24 -0300

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,9 +13,8 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-
-
+}dog;
+typedef dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
