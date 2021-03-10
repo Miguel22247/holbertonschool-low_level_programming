ed0e6e04df2874cf1e36d880cec683782a993a77
Miguel22247 <minipachru@gmail.com>
2021-03-10 10:42:03 -0300

    add files

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator - iterates an array
+ * @array: is an array
+ * @size: is the size 
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,6 @@
+#include "function_pointers.h"
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+    
+}
