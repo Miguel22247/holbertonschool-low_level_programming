8b677b3a2d19ded1e35cc64ffa789069bf415730
Miguel22247 <minipachru@gmail.com>
2021-03-10 14:27:56 +0000

    add 1-array_iterator.c and fix function_pointers.h

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,9 +3,18 @@
 /**
  * array_iterator - iterates an array
  * @array: is an array
- * @size: is the size 
- */
+ * @size: is the size
+ * @action: is something
+*/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i;
 
+	if (array == NULL)
+		return;
+	if (action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
