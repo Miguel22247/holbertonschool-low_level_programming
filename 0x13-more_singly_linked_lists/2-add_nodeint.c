dff2924884c296588696d40cd2c16b015a3c084d
Miguel22247 <minipachru@gmail.com>
2021-03-22 10:00:10 -0300

    added code

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * add_node - adds a node
+ * @head: is the head node
+ * @str: string
+ * Return: a new node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	int i, count = 0;
+	list_t *new;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+
+	new->len = i;
+	new->str = strdup(str);
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -57,7 +57,7 @@ Write a function that returns the number of elements in a linked listint_t list.
 Write a function that adds a new node at the beginning of a listint_t list.
 
 * Prototype: ```listint_t *add_nodeint(listint_t **head, const int n);```
-* Return: the address of the new element, or NULL if it faile### d
+* Return: the address of the new element, or NULL if it failed
 
  
 ### 3. Add node at the end
