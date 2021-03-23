9ef7f965a9d29cafd803e1b87838596c0c966d51
Miguel22247 <minipachru@gmail.com>
2021-03-23 09:46:42 -0300

    update

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,10 +1,8 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
- * free_listint - a function that frees a listint_t list.
- * @head: struct
- * Return: free
+ * free_listint - Entry Point
+ * @head: head
+ * Return: 0
  */
 void free_listint(listint_t *head)
 {
