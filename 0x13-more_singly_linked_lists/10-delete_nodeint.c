8e99d9b4487501f08a299273b7c3f9f11e67b46d
Miguel22247 <minipachru@gmail.com>
2021-03-23 10:03:04 -0300

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -5,38 +5,34 @@
  * @index: index
  * Return: 0
  */
+/**
+ * delete_nodeint_at_index - deletes a listint node at index
+ *
+ * @head: head of list
+ * @index: index to delete at
+ *
+ * Return: 1 if successful, -1 otherwise
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int count = 0;
+	listint_t *ptr, *prev;
 
-	listint_t *copy;
-	listint_t *temp;
-
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
-
-	copy = *head;
-
 	if (index == 0)
 	{
-		temp = copy->next;
-		free(copy);
-		*head = temp;
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
 		return (1);
 	}
 
-	while (copy != NULL)
-	{
-		if (count + 1 == index)
-		{
-			temp = copy->next;
-			copy->next = temp->next;
-			free(temp);
-			return (1);
-		}
-		if (copy->next != NULL)
-			copy = copy->next;
-		count++;
-	}
-	return (-1);
+	for (ptr = *head; index && ptr->next != NULL; index--, ptr = ptr->next)
+		prev = ptr;
+	if (index)
+		return (-1);
+
+	prev->next = prev->next->next;
+	free(ptr);
+	return (1);
 }
