71c45f8414822a2c56e44fd7f9a8d99b17c7da7c
Miguel22247 <minipachru@gmail.com>
2021-03-25 11:48:01 -0300

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * print_binary - Entry Point
+ * @n: dec input
+ * Return: 0
+ */
+void print_binary(unsigned long int n)
+{
+	int i = 0, count, k, temp;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	temp = n;
+
+	while (temp != 0)
+	{
+		i++;
+		temp = temp >> 1;
+	}
+
+	for (count = i - 1; count >= 0; count --)
+	{
+		k = n >> count;
+		if (k & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
+	
