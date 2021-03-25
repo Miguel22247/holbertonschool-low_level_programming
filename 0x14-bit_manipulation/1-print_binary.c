7cbc541a392371c73eb9b9cacfef67d44ea4a734
Miguel22247 <minipachru@gmail.com>
2021-03-25 14:09:46 -0300

    fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,30 +4,33 @@
  * @n: dec input
  * Return: 0
  */
+#include <unistd.h>
+#include <limits.h>
+/**
+ * print_binary - print an unsigned int in binary
+ *
+ * @n:
+ *
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
-	int i = 0, count, k, temp;
+	unsigned long int printbit = 1ul << 63;
+	char c = '0';
 
-	if (n == 0)
+	while (!(printbit & n) && printbit != 0)
+		printbit = printbit >> 1;
+
+	if (printbit == 0)
+		write(1, &c, 1);
+		
+	while (printbit)
 	{
-		printf("0");
-		return;
-	}
-
-	temp = n;
-
-	while (temp != 0)
-	{
-		i++;
-		temp = temp >> 1;
-	}
-
-	for (count = i - 1; count >= 0; count--)
-	{
-		k = n >> count;
-		if (k & 1)
-			printf("1");
+		if (printbit & n)
+			c = '1';
 		else
-			printf("0");
+			c = '0';
+		write(1, &c, 1);
+		printbit = printbit >> 1;
 	}
 }
