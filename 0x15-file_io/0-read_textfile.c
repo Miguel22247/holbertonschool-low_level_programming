e85c46c63d0017fd630e7c0396a8d0d68a0f29ac
Miguel22247 <minipachru@gmail.com>
2021-03-29 12:18:08 -0300

    Added Header file, readme and 0 file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - prints text from a file
+ *
+ * @filename: name of the file
+ * @letters: number of characters to read
+ *
+ * Return: actual number of letters read, 0 if end of file
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	int length, wrotechars;
+	char *buf;
+
+	if (filename == NULL || letters == 0)
+		return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (buf == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	length = read(file, buf, letters);
+	if (length == -1)
+	{
+		free(buf);
+		close(file);
+		return (0);
+	}
+
+	wrotechars = write(STDOUT_FILENO, buf, length);
+
+	free(buf);
+	close(file);
+	if (wrotechars != length)
+		return (0);
+	return (length);
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTONH
+#define HOLBERTONH
+
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+int _putchar(char c);
+
+#endif
