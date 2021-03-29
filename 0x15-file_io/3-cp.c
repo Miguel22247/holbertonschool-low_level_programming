2be8f9e92cb56d9dcdfabe2cba9c56e7f8fa27ac
Miguel22247 <minipachru@gmail.com>
2021-03-29 13:38:21 -0300

    added files and fixed some code

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+/**
+ * append_text_to_file - creates a file and puts text in it
+ * with 600 perms (do not change if it exists)
+ *
+ * @filename: name for file
+ * @text_content: text to put into file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t lenght = 0, inlen = 0;
+	char *ptr;
+	
