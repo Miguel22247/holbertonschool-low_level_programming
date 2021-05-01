28e49cd73ea6205289687d15a781032e6787423e
Miguel22247 <nr2862002@gmail.com>
2021-05-01 13:24:01 -0300

    added libraries to header

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,9 +1,12 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -21,6 +24,7 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
+int _putchar(char c);
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
