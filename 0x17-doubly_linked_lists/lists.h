86c0a336437207fc9aeaab77c81b059d7357e90d
Miguel22247 <nr2862002@gmail.com>
2021-05-01 10:31:59 -0300

    fixed betty

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -12,9 +12,9 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
