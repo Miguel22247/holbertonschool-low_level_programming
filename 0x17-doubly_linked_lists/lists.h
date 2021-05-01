95e6567b3129cbb0d1c426d3fc85bb321872a285
Miguel22247 <nr2862002@gmail.com>
2021-05-01 13:01:10 -0300

    fixed header

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -26,3 +26,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+#endif
