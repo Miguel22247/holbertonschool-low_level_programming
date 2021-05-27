8849f9be1764b466a6cfac02afb5a6d925192321
Miguel22247 <minipachru@gmail.com>
2021-05-27 10:55:10 -0300

    added excercise 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_table_create.h"
+/**
+* hash_table_create - create a new hash table
+* @size: size of the array
+* Return: a pointer to the new hash table
+*/
+
+hash_table_t *hash_table_create(unsigned long int size) {
+	unsigned long int index = 0;
+	hash_table_t *new_hash_table = NULL;
+
+	new_hash_table = malloc(sizeof(hash_table_t));
+	if (!new_hash_table)
+		return (NULL);
+	new_hash_table->array = malloc(sizeof(hash_node_t)* size);
+	if (!new_hash_table->array)
+	{
+		free(new_hash_table);
+		new_hash_table = NULL;
+		return (NULL);
+	}
+	while (index < size)
+	{
+		new_hash_table->array[index] = NULL
+		index++;
+	}
+	new_hash_table->size = size;
+	return (new_hash_table);
+}
