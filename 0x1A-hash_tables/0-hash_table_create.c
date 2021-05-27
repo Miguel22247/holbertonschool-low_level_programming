17cb4aa1e2009722b68cbeddb74a50603f5fa89b
Miguel22247 <minipachru@gmail.com>
2021-05-27 12:51:29 -0300

    returned to previous status

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,8 +7,9 @@
 
 hash_table_t *hash_table_create(unsigned long int size) {
 	unsigned long int index = 0;
-	hash_table_t *new_hash_table = malloc(sizeof(hash_table_t));
-	
+	hash_table_t *new_hash_table = NULL;
+
+	new_hash_table = malloc(sizeof(hash_table_t));
 	if (!new_hash_table)
 		return (NULL);
 	new_hash_table->array = malloc(sizeof(hash_node_t)* size);
