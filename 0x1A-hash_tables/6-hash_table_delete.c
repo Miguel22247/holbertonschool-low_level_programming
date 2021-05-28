8ed134ccc3bdcacccc612b46ecd2ee961e506be4
Miguel22247 <nr2862002@gmail.com>
2021-05-28 15:12:25 -0300

    fixed?

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -18,9 +18,10 @@ void hash_table_delete(hash_table_t *ht)
 
 	while (l < ht->size)
 	{
-		temporal = next_temporal;
+		temporal = ht->array[l];
 		while (temporal)
 		{
+			temporal = next_temporal
 			next_temporal = next_temporal->next;
 			if (temporal->key)
 				free(temporal->key);
