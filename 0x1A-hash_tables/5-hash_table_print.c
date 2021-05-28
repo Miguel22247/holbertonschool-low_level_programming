c38669931290265efe251ce184d4c324c9424f40
Miguel22247 <nr2862002@gmail.com>
2021-05-28 16:26:55 -0300

    fixed?

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,7 +7,7 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int index;
-	hash_node_t *ptr;
+	hash_node_t *temporal;
 	int first = 1;
 
 	if (!ht)
@@ -15,15 +15,15 @@ void hash_table_print(const hash_table_t *ht)
 	
 	printf("{");
 	for (index = 0; index < ht->size; index++)
-		for (prt = ht->array[index]; ptr != NULL; ptr = ptr->next)
+		for (temporal = ht->array[index]; temporal != NULL; temporal = temporal->next)
 		{
 			if (first)
 			{
-				printf("'%s': '%s'", ptr->key, ptr->value);
+				printf("'%s': '%s'", temporal->key, temporal->value);
 				first = 0;
 			}
 			else
-				printf(", '%s': '%s'", ptr->key, ptr->value);
+				printf(", '%s': '%s'", temporal->key, temporal->value);
 		}
 	printf("}\n");
 }
