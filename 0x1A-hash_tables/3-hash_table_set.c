138fa407b8e067e2cd0098ffd32e55e5b249ef60
Miguel22247 <minipachru@gmail.com>
2021-05-28 11:00:09 -0300

    added file 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,52 @@
+#include "hash_tables.h"
+/**
+* hash_table_set - add an element to the table
+* @ht: hash table
+* @key: key of the element
+* @value: value of the element
+* Return: an element added to the table
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0;
+	char *temporal_value = NULL;
+	hash_node_t *temporal = NULL;
+	hash_node_t *new = NULL;
+
+	if (!ht || !ht->array || !value)
+		return(0);
+	
+	if(strlen(key) == 0 || !key)
+		return (0);
+	temporal_value = strdup(value);
+	if (!temporal_value)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+
+	/* checks if a collision exists */
+	temporal = ht->array[index];
+	while (temporal)
+	{
+		if (strcmp(temporal->key, key) == 0)
+		{
+			free(temporal->value);
+			temp->value = temporal_value;
+			temp->value = strdup(value);
+			free(temporal_value);
+			return(1);
+		}
+		temporal = temp->next;
+	}
+	/* If a collision doesn't exits, insert node */
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+	{
+		free (new);
+		return (0);
+	}
+	new->key = strdup(key);
+	new->value = temporal_value;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
