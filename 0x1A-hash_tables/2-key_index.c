3ef7663e6e40887618b4d0e597af370a7ead5b37
Miguel22247 <minipachru@gmail.com>
2021-05-28 09:21:25 -0300

    fixed comments

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
-* key index - a function that gives the index of the key
+* key_index - a function that gives the index of the key
 * @key: key of the hash table
 * @size: size of the array
 * Return: the index of the key
