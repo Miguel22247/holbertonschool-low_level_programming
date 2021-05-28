e8754b2f0f0baf3d2a1ae5d1a8ba2a9533e72d6a
Miguel22247 <nr2862002@gmail.com>
2021-05-28 16:23:11 -0300

    added file for testing

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+* hash_table_print - prints the hash table
+* @ht: hash tables\
+* Return: a dictionary with the format of the hash table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int l;
+	hash_node_t *temporal = NULL;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	while (l < ht->size)
+	{
+		temporal = ht->array[l];
+		while (temporal)
+		{
+			printf("'%s': '%s'", temporal->key, temp->value);
+			if (!ht->array[i + 1])
+				break;
+			printf(", ");
+			temporal = temporal->next;
+		}
+		l++;
+	}
+	printf("\n");
+}
