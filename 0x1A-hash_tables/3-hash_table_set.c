705bcc9ef20753eee920a2fea5c291a6b56ca1f9
Miguel22247 <minipachru@gmail.com>
2021-05-28 11:03:40 -0300

    fixed compiler errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,8 +30,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(temporal->key, key) == 0)
 		{
 			free(temporal->value);
-			temp->value = temporal_value;
-			temp->value = strdup(value);
+			temporal->value = temporal_value;
+			temporal->value = strdup(value);
 			free(temporal_value);
 			return(1);
 		}
