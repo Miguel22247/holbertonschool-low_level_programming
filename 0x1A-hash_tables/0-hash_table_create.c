713dd51d42d7a752b11d03694d4f030f73fbe31b
Miguel22247 <nr2862002@gmail.com>
2021-05-28 15:26:13 -0300

    returned to previous state

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,19 +7,22 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_hash_table
+	unsigned long int index = 0;
+	hash_table_t *new_hash_table = malloc(sizeof(hash_table_t));
 
-	if (size <= 0)
-		return (NULL);
-	
-	new_hash_table  = malloc(sizeof(hash_table_t));
 	if (!new_hash_table)
 		return (NULL);
-
-	new_hash_table->size = size;
 	new_hash_table->array = malloc(sizeof(hash_node_t **) * size);
 	if (!new_hash_table->array)
+	{
+		free(new_hash_table);
 		return (NULL);
-	
-	return (hashtable);
+	}
+	while (index < size)
+	{
+		new_hash_table->array[index] = NULL;
+		index++;
+	}
+	new_hash_table->size = size;
+	return (new_hash_table);
 }
