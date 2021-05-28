138ac756448888e13af003702288ac13d0e60e5c
Miguel22247 <nr2862002@gmail.com>
2021-05-28 16:28:34 -0300

    fixed betty style

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -12,7 +12,7 @@ void hash_table_print(const hash_table_t *ht)
 
 	if (!ht)
 		return;
-	
+
 	printf("{");
 	for (index = 0; index < ht->size; index++)
 		for (temporal = ht->array[index]; temporal != NULL; temporal = temporal->next)
