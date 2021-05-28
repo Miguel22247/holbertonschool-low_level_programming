54150554452b1b755dfa9d1bdaae1dd7b4643a01
Miguel22247 <minipachru@gmail.com>
2021-05-28 09:07:53 -0300

    fixed betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,13 +5,14 @@
 * Return: a pointer to the new hash table
 */
 
-hash_table_t *hash_table_create(unsigned long int size) {
+hash_table_t *hash_table_create(unsigned long int size) 
+{
 	unsigned long int index = 0;
 	hash_table_t *new_hash_table = malloc(sizeof(hash_table_t));
 
 	if (!new_hash_table)
 		return (NULL);
-	new_hash_table->array = malloc(sizeof(hash_node_t)* size);
+	new_hash_table->array = malloc(sizeof(hash_node_t) * size);
 	if (!new_hash_table->array)
 	{
 		free(new_hash_table);
