d4d3aed3e0793b93dc5e52740027df61e8ac934b
Miguel22247 <minipachru@gmail.com>
2021-08-30 10:27:34 -0300

    add 0 for test

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algo.h"
+/**
+* linear-search -  linear search algorithm
+* @array: is a pointer to the array to search
+* @size: is the number of elements in array
+* @value: is the value to search
+* Return: 0 or -1
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0;
+	while (array)
+	{
+		print(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			if (array[i] == value)
+				return (array[i]);
+		}	
+	}
+	return (-1);
+}
