1fc031163d8ecc9f9d0313bd6717ebb3f6dfdfbe
Miguel22247 <minipachru@gmail.com>
2021-08-30 09:24:54 -0300

    add header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/* Libraries */
+#include <stdio.h>
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
